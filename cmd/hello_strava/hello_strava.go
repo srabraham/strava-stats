bab963c26d836ae7cc48f70afa1d1729fe6adbd1
Sean Abraham <seanabraham@gmail.com>
2019-03-17 18:42:40 -0600

    Initial check-in of Strava API demo code

---
+++ cmd/hello_strava/hello_strava.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/srabraham/strava-oauth-helper/stravaauth"
+
+	strava "github.com/srabraham/swagger-strava-go"
+)
+
+func main() {
+	flag.Parse()
+
+	scopes := []string{"read_all", "activity:read_all", "profile:read_all"}
+	oauthCtx, err := stravaauth.GetOAuth2Ctx(context.Background(), strava.ContextOAuth2, scopes)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client := strava.NewAPIClient(strava.NewConfiguration())
+	athlete, _, err := client.AthletesApi.GetLoggedInAthlete(oauthCtx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Got athlete:")
+	spew.Dump(athlete)
+
+	activities := make([]strava.SummaryActivity, 0)
+	for i := 1; ; i++ {
+		activitiesPage, _, err := client.ActivitiesApi.GetLoggedInAthleteActivities(
+			oauthCtx,
+			map[string]interface{}{
+				"page": int32(i),
+				// 200 appears to be the most Strava will let you grab per call.
+				"perPage": int32(200),
+			},
+		)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if len(activitiesPage) == 0 {
+			break
+		}
+		activities = append(activities, activitiesPage...)
+	}
+	for _, a := range activities {
+		dur, err := time.ParseDuration(fmt.Sprintf("%ds", a.MovingTime))
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("%s: %s: %.2fkm: %v\n", a.Name, a.StartDateLocal, a.Distance/1e3, dur)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/srabraham/strava-stats
+
+require (
+	github.com/davecgh/go-spew v1.1.1
+	github.com/srabraham/strava-oauth-helper v0.0.0-20190317234731-d44211d0dd56
+	github.com/srabraham/swagger-strava-go v0.0.0-20190310022918-a9f422243df0
+	golang.org/x/net v0.0.0-20190310014029-b774fd8d5c0f // indirect
+)
