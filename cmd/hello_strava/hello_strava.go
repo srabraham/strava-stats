9d0008ff82fe839558fdb94346daf55a55f5f9f5
Sean Abraham <seanabraham@gmail.com>
2019-03-18 22:41:41 -0600

    Refactors and cleanups

--- cmd/hello_strava/hello_strava.go
+++ cmd/hello_strava/hello_strava.go
@@ -16,6 +16,87 @@ import (
 	strava "github.com/srabraham/swagger-strava-go"
 )
 
+func main() {
+	flag.Parse()
+
+	// Do all the auth stuff first
+	gClient := getGoogleClient()
+	stravaScopes := []string{"read_all", "activity:read_all", "profile:read_all"}
+	oauthCtx, err := stravaauth.GetOAuth2Ctx(context.Background(), strava.ContextOAuth2, stravaScopes)
+	if err != nil {
+		log.Fatal(err)
+	}
+	sClient := strava.NewAPIClient(strava.NewConfiguration())
+
+	// Fetch things from Strava
+	athlete := *getLoggedInAthleteProfile(sClient, &oauthCtx)
+	activities := *getLoggedInAthleteActivities(sClient, &oauthCtx)
+
+	// Create a new Spreadsheet and populate it with the Strava data
+	sheetsService, err := sheets.New(gClient)
+	if err != nil {
+		log.Fatal(err)
+	}
+	ss := createStatsSpreadsheet(&athlete, &activities)
+	resp, err := sheetsService.Spreadsheets.Create(ss).Context(context.Background()).Do()
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Resize the Sheet to make the columns wide enough.
+	_, err = sheetsService.Spreadsheets.BatchUpdate(
+		resp.SpreadsheetId,
+		&sheets.BatchUpdateSpreadsheetRequest{
+			Requests: []*sheets.Request{
+				{AutoResizeDimensions: &sheets.AutoResizeDimensionsRequest{
+					Dimensions: &sheets.DimensionRange{
+						SheetId:   resp.Sheets[0].Properties.SheetId,
+						Dimension: "COLUMNS"}}}}}).Do()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Response = %v", resp)
+	log.Printf("Spreadsheet is at %s", resp.SpreadsheetUrl)
+}
+
+func getLoggedInAthleteProfile(sClient *strava.APIClient, oauthCtx *context.Context) *strava.DetailedAthlete {
+	athlete, _, err := sClient.AthletesApi.GetLoggedInAthlete(*oauthCtx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Got athlete:")
+	spew.Dump(athlete)
+	return &athlete
+}
+
+func getLoggedInAthleteActivities(sClient *strava.APIClient, oauthCtx *context.Context) *[]strava.SummaryActivity {
+	// Fetch all of the logged-in athlete's activities, 200 at a time.
+	activities := make([]strava.SummaryActivity, 0)
+	for i := 1; ; i++ {
+		activitiesPage, _, err := sClient.ActivitiesApi.GetLoggedInAthleteActivities(
+			*oauthCtx,
+			map[string]interface{}{
+				"page":    int32(i),
+				"perPage": int32(200),
+			},
+		)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if len(activitiesPage) == 0 {
+			// No more Strava activities to fetch.
+			break
+		}
+		log.Printf("Got page %d of activities", i)
+		activities = append(activities, activitiesPage...)
+	}
+	log.Printf("Read %d activities", len(activities))
+	if len(activities) > 0 {
+		log.Println("Most recent activity:")
+		spew.Dump(activities[0])
+	}
+	return &activities
+}
+
 func getGoogleClient() *http.Client {
 	if err := googleauth.AddScope("https://www.googleapis.com/auth/spreadsheets"); err != nil {
 		log.Fatal(err)
@@ -30,54 +111,7 @@ func getGoogleClient() *http.Client {
 	return googleClient
 }
 
-func main() {
-	flag.Parse()
-
-	scopes := []string{"read_all", "activity:read_all", "profile:read_all"}
-	oauthCtx, err := stravaauth.GetOAuth2Ctx(context.Background(), strava.ContextOAuth2, scopes)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	gClient := getGoogleClient()
-
-	sClient := strava.NewAPIClient(strava.NewConfiguration())
-	athlete, _, err := sClient.AthletesApi.GetLoggedInAthlete(oauthCtx)
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("Got athlete:")
-	spew.Dump(athlete)
-
-	activities := make([]strava.SummaryActivity, 0)
-	for i := 1; ; i++ {
-		activitiesPage, _, err := sClient.ActivitiesApi.GetLoggedInAthleteActivities(
-			oauthCtx,
-			map[string]interface{}{
-				"page":    int32(i),
-				"perPage": int32(200),
-			},
-		)
-		if err != nil {
-			log.Fatal(err)
-		}
-		if len(activitiesPage) == 0 {
-			break
-		}
-		log.Printf("Got page %d of activities", i)
-		activities = append(activities, activitiesPage...)
-	}
-	log.Printf("Read %d activities", len(activities))
-	if len(activities) > 0 {
-		log.Println("Most recent activity:")
-		spew.Dump(activities[0])
-	}
-
-	sheetsService, err := sheets.New(gClient)
-	if err != nil {
-		log.Fatal(err)
-	}
-
+func createStatsSpreadsheet(athlete *strava.DetailedAthlete, activities *[]strava.SummaryActivity) *sheets.Spreadsheet {
 	rowData := make([]*sheets.RowData, 0)
 	rowData = append(rowData, &sheets.RowData{
 		Values: []*sheets.CellData{
@@ -94,11 +128,14 @@ func main() {
 				StringValue: "Moving time",
 			}},
 			{UserEnteredValue: &sheets.ExtendedValue{
+				StringValue: "Elevation gain (m)",
+			}},
+			{UserEnteredValue: &sheets.ExtendedValue{
 				StringValue: "Activity name",
 			}},
 		}},
 	)
-	for _, a := range activities {
+	for _, a := range *activities {
 		// dur, err := time.ParseDuration(fmt.Sprintf("%ds", a.MovingTime))
 		// if err != nil {
 		// 	log.Fatal(err)
@@ -146,6 +183,17 @@ func main() {
 				},
 				{
 					UserEnteredValue: &sheets.ExtendedValue{
+						NumberValue: float64(a.TotalElevationGain),
+					},
+					UserEnteredFormat: &sheets.CellFormat{
+						NumberFormat: &sheets.NumberFormat{
+							Pattern: "0.0",
+							Type:    "NUMBER",
+						},
+					},
+				},
+				{
+					UserEnteredValue: &sheets.ExtendedValue{
 						StringValue: a.Name,
 					},
 				},
@@ -153,7 +201,7 @@ func main() {
 		})
 	}
 
-	rb := &sheets.Spreadsheet{
+	ss := &sheets.Spreadsheet{
 		Properties: &sheets.SpreadsheetProperties{
 			Title: fmt.Sprintf("%s Strava activities for %s %s", time.Now().Format("2006-01-02"), athlete.Firstname, athlete.Lastname),
 		},
@@ -168,10 +216,5 @@ func main() {
 			},
 		}},
 	}
-	resp, err := sheetsService.Spreadsheets.Create(rb).Context(context.Background()).Do()
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("Response = %v", resp)
-	log.Printf("Spreadsheet is at %s", resp.SpreadsheetUrl)
+	return ss
 }
