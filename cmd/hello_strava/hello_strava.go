a02a940b502ffdca16e29d37788ae8a5b60e6641
Sean Abraham <seanabraham@gmail.com>
2019-03-18 21:54:32 -0600

    Write Strava stats to a new Google Sheet

--- cmd/hello_strava/hello_strava.go
+++ cmd/hello_strava/hello_strava.go
@@ -5,13 +5,30 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"net/http"
 	"time"
 
 	"github.com/davecgh/go-spew/spew"
+	"github.com/srabraham/google-oauth-helper/googleauth"
 	"github.com/srabraham/strava-oauth-helper/stravaauth"
+	"google.golang.org/api/sheets/v4"
 
 	strava "github.com/srabraham/swagger-strava-go"
 )
+
+func getGoogleClient() *http.Client {
+	if err := googleauth.AddScope("https://www.googleapis.com/auth/spreadsheets"); err != nil {
+		log.Fatal(err)
+	}
+	if err := googleauth.SetTokenFileName("hellostrava-tok"); err != nil {
+		log.Fatal(err)
+	}
+	googleClient, err := googleauth.GetClient()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return googleClient
+}
 
 func main() {
 	flag.Parse()
@@ -22,8 +39,10 @@ func main() {
 		log.Fatal(err)
 	}
 
-	client := strava.NewAPIClient(strava.NewConfiguration())
-	athlete, _, err := client.AthletesApi.GetLoggedInAthlete(oauthCtx)
+	gClient := getGoogleClient()
+
+	sClient := strava.NewAPIClient(strava.NewConfiguration())
+	athlete, _, err := sClient.AthletesApi.GetLoggedInAthlete(oauthCtx)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -32,7 +51,7 @@ func main() {
 
 	activities := make([]strava.SummaryActivity, 0)
 	for i := 1; ; i++ {
-		activitiesPage, _, err := client.ActivitiesApi.GetLoggedInAthleteActivities(
+		activitiesPage, _, err := sClient.ActivitiesApi.GetLoggedInAthleteActivities(
 			oauthCtx,
 			map[string]interface{}{
 				"page":    int32(i),
@@ -48,11 +67,111 @@ func main() {
 		log.Printf("Got page %d of activities", i)
 		activities = append(activities, activitiesPage...)
 	}
-	for _, a := range activities {
-		dur, err := time.ParseDuration(fmt.Sprintf("%ds", a.MovingTime))
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Printf("%s, %.2fkm, %v, %s\n", a.StartDateLocal.Format("2006-01-02"), a.Distance/1e3, dur, a.Name)
+	log.Printf("Read %d activities", len(activities))
+	if len(activities) > 0 {
+		log.Println("Most recent activity:")
+		spew.Dump(activities[0])
 	}
+
+	sheetsService, err := sheets.New(gClient)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	rowData := make([]*sheets.RowData, 0)
+	rowData = append(rowData, &sheets.RowData{
+		Values: []*sheets.CellData{
+			{UserEnteredValue: &sheets.ExtendedValue{
+				StringValue: "Date",
+			}},
+			{UserEnteredValue: &sheets.ExtendedValue{
+				StringValue: "Type",
+			}},
+			{UserEnteredValue: &sheets.ExtendedValue{
+				StringValue: "Distance (km)",
+			}},
+			{UserEnteredValue: &sheets.ExtendedValue{
+				StringValue: "Moving time",
+			}},
+			{UserEnteredValue: &sheets.ExtendedValue{
+				StringValue: "Activity name",
+			}},
+		}},
+	)
+	for _, a := range activities {
+		// dur, err := time.ParseDuration(fmt.Sprintf("%ds", a.MovingTime))
+		// if err != nil {
+		// 	log.Fatal(err)
+		// }
+		// fmt.Printf("%s, %.2fkm, %v, %s\n", a.StartDateLocal.Format("2006-01-02"), a.Distance/1e3, dur, a.Name)
+		rowData = append(rowData, &sheets.RowData{
+			Values: []*sheets.CellData{
+				{
+					UserEnteredValue: &sheets.ExtendedValue{
+						StringValue: a.StartDateLocal.Format("2006-01-02"),
+					},
+					UserEnteredFormat: &sheets.CellFormat{
+						NumberFormat: &sheets.NumberFormat{
+							Pattern: "yyyy-mm-dd",
+							Type:    "DATE",
+						},
+					},
+				},
+				{
+					UserEnteredValue: &sheets.ExtendedValue{
+						StringValue: string(*a.Type_),
+					},
+				},
+				{
+					UserEnteredValue: &sheets.ExtendedValue{
+						NumberValue: float64(a.Distance) / 1e3,
+					},
+					UserEnteredFormat: &sheets.CellFormat{
+						NumberFormat: &sheets.NumberFormat{
+							Pattern: "0.00",
+							Type:    "NUMBER",
+						},
+					},
+				},
+				{
+					UserEnteredValue: &sheets.ExtendedValue{
+						NumberValue: float64(a.MovingTime) / (24 * 60 * 60),
+					},
+					UserEnteredFormat: &sheets.CellFormat{
+						NumberFormat: &sheets.NumberFormat{
+							Pattern: "[h]:mm:ss",
+							Type:    "TIME",
+						},
+					},
+				},
+				{
+					UserEnteredValue: &sheets.ExtendedValue{
+						StringValue: a.Name,
+					},
+				},
+			},
+		})
+	}
+
+	rb := &sheets.Spreadsheet{
+		Properties: &sheets.SpreadsheetProperties{
+			Title: fmt.Sprintf("%s Strava activities for %s %s", time.Now().Format("2006-01-02"), athlete.Firstname, athlete.Lastname),
+		},
+		Sheets: []*sheets.Sheet{{
+			Data: []*sheets.GridData{{
+				RowData: rowData,
+			}},
+			Properties: &sheets.SheetProperties{
+				GridProperties: &sheets.GridProperties{
+					FrozenRowCount: 1,
+				},
+			},
+		}},
+	}
+	resp, err := sheetsService.Spreadsheets.Create(rb).Context(context.Background()).Do()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Response = %v", resp)
+	log.Printf("Spreadsheet is at %s", resp.SpreadsheetUrl)
 }
--- go.mod
+++ go.mod
@@ -2,7 +2,8 @@ module github.com/srabraham/strava-stats
 
 require (
 	github.com/davecgh/go-spew v1.1.1
+	github.com/srabraham/google-oauth-helper v0.0.0-20190319023854-61358e3b7543
 	github.com/srabraham/strava-oauth-helper v0.0.0-20190319004537-6fa2bae7c3ca
 	github.com/srabraham/swagger-strava-go v0.0.0-20190318033004-3e5afe04cf23
-	golang.org/x/net v0.0.0-20190310014029-b774fd8d5c0f // indirect
+	google.golang.org/api v0.2.0
 )
