dd01d616c9835f7a6413e3b13ebbe0ff4cbed51c
Sean Abraham <seanabraham@gmail.com>
2022-10-09 17:34:33 -0600

    add overall timeout

--- cmd/hello_strava/hello_strava.go
+++ cmd/hello_strava/hello_strava.go
@@ -23,6 +23,7 @@ import (
 var (
 	athleteOutFile    = flag.String("athlete-out-file", "", "File in which to spew athlete details, or blank to not output such a file")
 	activitiesOutFile = flag.String("activities-out-file", "", "File in which to spew out all activities, or blank to not output such a file")
+	timeout           = flag.Duration("timeout", 30*time.Minute, "an overall timeout on the program")
 	workoutType       = map[int32]string{
 		0:  "Run",
 		1:  "Foot race",
@@ -36,12 +37,13 @@ var (
 
 func main() {
 	flag.Parse()
-	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(context.Background(), *timeout)
+	defer cancel()
 
 	// Do all the auth stuff first
 	gClient := getGoogleClient()
 	stravaScopes := []string{"read_all", "activity:read_all", "profile:read_all"}
-	oauthCtx, err := stravaauth.GetOAuth2Ctx(context.Background(), strava.ContextOAuth2, stravaScopes)
+	oauthCtx, err := stravaauth.GetOAuth2Ctx(ctx, strava.ContextOAuth2, stravaScopes)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -69,7 +71,7 @@ func main() {
 		log.Fatal(err)
 	}
 	ss := createStatsSpreadsheet(&athlete, &activities)
-	resp, err := sheetsService.Spreadsheets.Create(ss).Context(context.Background()).Do()
+	resp, err := sheetsService.Spreadsheets.Create(ss).Context(ctx).Do()
 	if err != nil {
 		log.Fatal(err)
 	}
